868372394c4b071c1c3d504b5812a0a8f351f72d
WIN-FI93QU5E7B9\Asidletska <nastiasidletskaya@gmail.com>
2024-07-16 21:04:37 +0300

    Game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] players;
 
-    // Update is called once per frame
-    void Update()
+    public void CheckWinState()
     {
-        
+        int aliveCount = 0;
+
+        foreach (GameObject player in players)
+        {
+            if (player.activeSelf)
+            {
+                aliveCount++;
+            }
+        }
+        if (aliveCount <= 1)
+        {
+            Invoke(nameof(NewRound), 3f);
+        }
+    }
+    private void NewRound()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
